ccf167998e02019372a5907b72b26cf692e94adb
rixuanzheng <rixuanzheng1@gmail.com>
2021-12-06 13:02:33 -0500

    added login if not logged in to private pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
@@ -6,6 +7,7 @@ import Home from './pages/Home';
 import SingleTeamEval from './pages/SingleTeamEval';
 import NotFound from './pages/NotFound';
 import Login from './pages/Login';
+import MyTeams from './pages/MyTeams';
 
 import Navbar from './components/navbar/Navbar';
 
@@ -19,7 +21,7 @@ function App() {
           <Route exact path="/home" element={<Home />} /> 
           <Route exact path="/single-team-eval" element={<SingleTeamEval />}/>
           <Route exact path="/login" element={<Login />}/>
-          <Route element={<NotFound />}/>
+          <Route exact path="/my-teams" element={<MyTeams />}/>
 
           <Route path="/404" element={<NotFound />} />
           <Route path="*" element={<Navigate replace to="/404" />} />
